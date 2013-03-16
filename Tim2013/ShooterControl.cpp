733307eb5c1736b8536cab61053679c823fdd186
komnoki <komnoki@7915cf92-1267-42b0-943f-3348142f1875>
2013-03-16 19:59:31 +0000

    Final Code.

--- Tim2013/ShooterControl.cpp
+++ Tim2013/ShooterControl.cpp
@@ -30,12 +30,13 @@ ShooterControl::ShooterControl() {
 	upperLimit = new DigitalInput(UPPERLIMITPORT);
 	lowerLimit = new DigitalInput(LOWERLIMITPORT);
 	shooterStartSpeed = .4;
-	
+	isLoadingPosition = false;
+	isFiringPosition = false;
 }
 
 void ShooterControl::initialize() {
 	Angle = 0.0;
-Stop();
+	Stop();
 }
 
 //void ShooterControl::initializeAutonomous() {
@@ -43,47 +44,92 @@ Stop();
 //}
 
 //this method cycles though the shooter speeds in 4 steps
-void ShooterControl::ShooterSeperateCycleSpeed(){
+void ShooterControl::ShooterSeperateCycleSpeed() {
 	bool isLBumperPressed = xbox->isLBumperPressed();
 	if (isLBumperPressed) {
 		Shooter1Speed += SHOOTERSPEEDSTEP;
-			if (Shooter1Speed > 1)
-				Shooter1Speed = 0;
+		if (Shooter1Speed > 1)
+			Shooter1Speed = 0;
 
-
-			
-		}
+	}
 	bool isRBumperPressed = xbox->isRBumperPressed();
 
-		if (isRBumperPressed) {
-			
-			
-			Shooter2Speed += SHOOTERSPEEDSTEP;
-			if (Shooter2Speed > 1)
-				Shooter2Speed = 0;
-		}
-		dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "shooter1speed: %f",
-				Shooter1Speed);
-		dsLCD->PrintfLine(DriverStationLCD::kUser_Line5, "shooter2speed: %f",
-				Shooter2Speed);
-		dsLCD->UpdateLCD();
+	if (isRBumperPressed) {
 
-		shooterMotor1->Set(Shooter1Speed);
-		shooterMotor2->Set(Shooter2Speed);
+		Shooter2Speed += SHOOTERSPEEDSTEP;
+		if (Shooter2Speed > 1)
+			Shooter2Speed = 0;
+	}
+	dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "s1: %f, s2: %f",
+			Shooter1Speed, Shooter2Speed);
+	dsLCD->UpdateLCD();
+
+	shooterMotor1->Set(Shooter1Speed);
+	shooterMotor2->Set(Shooter2Speed);
 
 }
-void ShooterControl::Stop(){
+void ShooterControl::Stop() {
 	Shooter1Speed = 0;
 	Shooter2Speed = 0;
 	shooterMotor1->Set(Shooter1Speed);
 	shooterMotor2->Set(Shooter2Speed);
 }
-void ShooterControl::APushStop(){
+void ShooterControl::APushStop() {
 	bool isAPressed = xbox->isAPressed();
-	if (isAPressed){
+	if (isAPressed) {
 		Stop();
+		isLoadingPosition = false;
+		isFiringPosition = false;
+
 	}
 }
+
+void ShooterControl::ShooterCycleBehindSpeed() {
+
+	bool isLBumperPressed = xbox->isLBumperPressed();
+
+	// if left bumper is pressed reduce the speed for both motors
+	if (isLBumperPressed) {
+		Shooter2Speed = (Shooter2Speed -= SHOOTERSPEEDSTEP) < 0 ? 0
+				: Shooter2Speed;
+		// if shooter speed is less than shooterstart speed. stop
+		if (Shooter2Speed < shooterStartSpeed)
+			Shooter2Speed = 0;
+
+		// Both variables are set to the same speed, but we are using two variables incase we want to set two different values.
+		// From final code extra variable can be removed
+	}
+
+	// if right bumper is pressed increase the speed for both motors
+	bool isRBumperPressed = xbox->isRBumperPressed();
+
+	if (isRBumperPressed) {
+
+		Shooter2Speed = (Shooter2Speed += SHOOTERSPEEDSTEP) > 1.0 ? 1
+				: Shooter2Speed;
+
+		// if shooter speed is equal to first speed increment (.25) jump to shooter start speed (.4).
+		if (Shooter2Speed == SHOOTERSPEEDSTEP)
+			Shooter2Speed = shooterStartSpeed;
+
+		// Both variables are set to the same speed, but we are using two variables incase we want to set two different values.
+		// From final code extra variable can be removed
+	}
+	if (Shooter2Speed < .3) {
+		Shooter1Speed = Shooter2Speed;
+	} else {
+		Shooter1Speed = Shooter2Speed - SHOOTERSPEEDSTEPDIFF;
+	}
+
+	dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "shooterspeed: %f",
+			Shooter2Speed);
+	dsLCD->UpdateLCD();
+
+	shooterMotor1->Set(Shooter1Speed);
+	shooterMotor2->Set(Shooter2Speed);
+
+}
+
 void ShooterControl::ShooterCycleSpeed() {
 
 	bool isLBumperPressed = xbox->isLBumperPressed();
@@ -105,8 +151,7 @@ void ShooterControl::ShooterCycleSpeed() {
 	bool isRBumperPressed = xbox->isRBumperPressed();
 
 	if (isRBumperPressed) {
-		
-		
+
 		Shooter1Speed = (Shooter1Speed += SHOOTERSPEEDSTEP) > 1.0 ? 1
 				: Shooter1Speed;
 
@@ -119,17 +164,14 @@ void ShooterControl::ShooterCycleSpeed() {
 		Shooter2Speed = Shooter1Speed;
 	}
 
-	dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "shooterspeed: %f",
-			Shooter1Speed);
-	dsLCD->UpdateLCD();
-
 	shooterMotor1->Set(Shooter1Speed);
 	shooterMotor2->Set(Shooter2Speed);
-
+	dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "s1: %f s2:%f",
+			shooterMotor1->Get(), shooterMotor2->Get());
+	dsLCD->UpdateLCD();
 }
 
-bool ShooterControl::isLowestAngle()
-{
+bool ShooterControl::isLowestAngle() {
 	return !lowerLimit->Get();
 }
 // this method sets the angle of the shooter using a motor. elevation is increased when right trigger is pressed, and it is decreased
@@ -137,19 +179,21 @@ void ShooterControl::ShooterAngle(float angleDirection) {
 	//rightAngle = xbox->getAxisRightY();
 	bool upperOn = !upperLimit->Get();
 	bool lowerOn = !lowerLimit->Get();
-	
+
 	// if we are hitting the limit, cancel the direction so that we don't move the motor
 	if (lowerOn) {
 		if (angleDirection < 0) {
 			angleDirection = 0;
+			isLoadingPosition = false;
 		}
 	}
-	
+
 	if (upperOn) {
 		if (angleDirection > 0)
 			angleDirection = 0;
+		isFiringPosition = false;
 	}
-	
+
 	// check for dead zone i.e +- 1. move motor if beyond dead zone
 	if (fabs(angleDirection) > .1) {
 		AngleMotor->Set(-1 * angleDirection);
@@ -157,9 +201,9 @@ void ShooterControl::ShooterAngle(float angleDirection) {
 	} else {
 		AngleMotor->Set(0.0);
 	}
-	
-	dsLCD->PrintfLine(DriverStationLCD::kUser_Line3, "U: %i L: %i",
-				upperOn, lowerOn);
+
+	dsLCD->PrintfLine(DriverStationLCD::kUser_Line3, "U: %i L: %i", upperOn,
+			lowerOn);
 	dsLCD->UpdateLCD();
 }
 
@@ -172,34 +216,65 @@ bool ShooterControl::isRunning() {
 
 }
 
-
-void ShooterControl::SetShooterMotors(float speed){
+void ShooterControl::SetShooterMotors(float speed) {
 	shooterMotor1->Set(speed);
-	shooterMotor2->Set(speed);	
+	shooterMotor2->Set(speed);
 }
 
-float ShooterControl::getAngle()
-{
+float ShooterControl::getAngle() {
 	// use this for now
 	return maxAngleReached();
 }
 
-bool ShooterControl::maxAngleReached()
-{
+bool ShooterControl::maxAngleReached() {
 	return !upperLimit->Get();
 }
 
 //press right trigger to shoot
 void ShooterControl::run() {
 	APushStop();
-	ShooterSeperateCycleSpeed();
-	//ShooterCycleSpeed();
-	ShooterAngle(xbox->getAxisRightY());
+	//ShooterSeperateCycleSpeed();float axisY = xbox->getAxisRightY();
+	goTo();
+	float axisY = xbox->getAxisRightY();
+	if (fabs(axisY) > .2) {
+		isLoadingPosition = false;
+		isFiringPosition = false;
+	}
+	if (isLoadingPosition)//loading position
+	{
+		ShooterAngle(SHOOTERLOADINGDIRECTION);
+		Shooter1Speed = SHOOTERLOADINGSPEED;
+		Shooter2Speed = SHOOTERLOADINGSPEED;
+	} else if (isFiringPosition) {
+		ShooterAngle(SHOOTERFIRINGDIRECTION);
+		Shooter1Speed = SHOOTERFIRINGSPEED;
+		Shooter2Speed = SHOOTERFIRINGSPEED;
+	} else {
 
+		//ShooterCycleBehindSpeed();
+		ShooterAngle(axisY);
+		if (xbox->isXPressed()) {
+			Shooter2Speed = DEFAULTSHOOTERSPEED;
+			Shooter1Speed = DEFAULTSHOOTERSPEED;
+		}
+	}
+	ShooterCycleSpeed();
 }
 //void ShooterControl::runAutonomous() {
 //shooterMotor1->Set(AUTOSPEED);
 //shooterMotor2->Set(AUTOSPEED);
 //
 //}
+/*
+ * if Y is pressed, goes to loading position, turns motors off
+ * If B is pressed, goes to firing position, turns motors on
+ */
+void ShooterControl::goTo() {
+	if (xbox->isYPressed()) {
+		isLoadingPosition = true;
+	} else if (xbox->isBPressed()) {
+		isFiringPosition = true;
+	}
+
+}
 
