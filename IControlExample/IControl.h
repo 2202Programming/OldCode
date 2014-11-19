2e66d37647f71db0cbf9b9986f7c00f1ec23b459
komnoki <komnoki@7915cf92-1267-42b0-943f-3348142f1875>
2014-11-19 02:06:34 +0000

    LCD renamed for examples

---
+++ IControlExample/IControl.h
@@ -0,0 +1,16 @@
+#ifndef ICONTROL_H
+#define ICONTROL_H
+
+
+class IControl {
+public:
+	IControl(){}
+	virtual void initialize() = 0;
+	virtual void initializeAuto() = 0;
+	virtual void run() = 0;
+	virtual void runAuto() = 0;
+	
+private:
+	
+};
+#endif
